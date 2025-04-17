a80ebcac182d11f1d4aa8c4909d81db76532ecdd
haoChenGIS <ch2662353032@gmail.com>
2025-04-17 17:08:58 +0800

    Use CSS Modules

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
 import PageNav from "../ components/PageNav";
+import AppNav from "../ components/AppNav";
 function Homepage() {
   return (
     <div>
       <PageNav />
-      <h1>Worldwise</h1>
+      <AppNav />
+      <h1 className="test">Worldwise</h1>
+      <Link to="/app"> Go to the app</Link>
     </div>
   );
 }
